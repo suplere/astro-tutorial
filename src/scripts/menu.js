f4932b4d862afb4ec108036991ff7e0ce9ef3234
Evzen Supler <evzen.supler@seznam.cz>
2024-02-08 15:47:49 +0100

    add transiction

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,8 @@
 document.querySelector('.hamburger').addEventListener('click', () => {
   document.querySelector('.nav-links').classList.toggle('expanded');
 });
+document.addEventListener('astro:page-load', () => {
+  document.querySelector('.hamburger').addEventListener('click', () => {
+    document.querySelector('.nav-links').classList.toggle('expanded');
+  });
+});
